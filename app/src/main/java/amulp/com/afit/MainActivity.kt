ad014a3693b0442aad7f24e1880c3163f6ff4ca1
Amul Pokharel <amulpokh@gmail.com>
2018-08-13 16:44:51 -0400

    fragment container + default fragment

--- app/src/main/java/amulp/com/afit/MainActivity.kt
+++ app/src/main/java/amulp/com/afit/MainActivity.kt
@@ -1,12 +1,17 @@
 package amulp.com.afit
 
+import amulp.com.afit.ui.main.AddExerciseFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, AddExerciseFragment.newInstance())
+                    .commitNow()
+        }
     }
 }
