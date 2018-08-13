bd23c30091b42de89d76ff49cca7e543842c3a56
Amul Pokharel <amulpokh@gmail.com>
2018-08-13 16:45:07 -0400

    onclick for the add button

--- app/src/main/java/amulp/com/afit/ui.main/AddExerciseFragment.kt
+++ app/src/main/java/amulp/com/afit/ui.main/AddExerciseFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.add_exercise_fragment.*
 
 class AddExerciseFragment: Fragment() {
 
@@ -23,5 +24,12 @@ class AddExerciseFragment: Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        add_exercise.setOnClickListener { addExercise() }
     }
+
+    private fun addExercise(){
+        
+    }
+
 }
