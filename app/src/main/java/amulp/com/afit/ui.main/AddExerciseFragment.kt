82ab9c97ddf3e88a4d8bc3eaf2cef13150f8e98b
Amul Pokharel <amulpokh@gmail.com>
2018-08-13 16:08:44 -0400

    added content to add exercise fragment

--- app/src/main/java/amulp/com/afit/ui.main/AddExerciseFragment.kt
+++ app/src/main/java/amulp/com/afit/ui.main/AddExerciseFragment.kt
@@ -1,7 +1,27 @@
 package amulp.com.afit.ui.main
 
+import amulp.com.afit.R
+import amulp.com.afit.utils.inflate
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 
 class AddExerciseFragment: Fragment() {
 
+    companion object {
+        private var fragmentType:String = ""
+        fun newInstance() = AddExerciseFragment()
+    }
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+            container!!.inflate(R.layout.add_exercise_fragment)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
 }
