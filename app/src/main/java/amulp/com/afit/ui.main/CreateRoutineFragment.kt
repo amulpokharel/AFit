9538dab58c98f9b51638751c97ccd0a5f6b82617
Amul Pokharel <amulpokh@gmail.com>
2018-09-17 15:12:18 -0400

    fragment to create new routines

--- app/src/main/java/amulp/com/afit/ui.main/CreateRoutineFragment.kt
+++ app/src/main/java/amulp/com/afit/ui.main/CreateRoutineFragment.kt
@@ -1,7 +1,116 @@
 package amulp.com.afit.ui.main
 
+import amulp.com.afit.R
+import amulp.com.afit.adapters.RecycleAdapter
+import amulp.com.afit.utils.parseString
+import amulp.com.afit.utils.showKeyboard
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.add_exercise_dialog.view.*
+import kotlinx.android.synthetic.main.list_fragment.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 
-class CreateRoutineFragment:Fragment() {
+class CreateRoutineFragment: Fragment(), LifecycleOwner {
+    companion object {
+        fun newInstance() = CreateRoutineFragment()
+    }
 
+    private lateinit var viewModel: MainViewModel
+    private lateinit var recycleAdapter: RecycleAdapter
+    private lateinit var dialogView: View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        viewModel.getExercisesLive().observe(this@CreateRoutineFragment, Observer { exercises ->
+            recycleAdapter.setData(exercises)
+        })
+
+        doAsync {
+            recycleAdapter = RecycleAdapter(viewModel.getAllExercises()) {
+                deleteExercise(it.name)
+            }
+
+            recyclerView.adapter = recycleAdapter
+        }
+
+        fab.setOnClickListener { showAddDialog() }
+
+    }
+
+    private fun showAddDialog(){
+        val layoutInflater = LayoutInflater.from(context)
+        dialogView = layoutInflater.inflate(R.layout.add_routine_dialog, null)
+        val alertBuilder = AlertDialog.Builder(context!!)
+
+        alertBuilder.setView(dialogView)
+
+        alertBuilder
+                .setCancelable(false)
+                .setPositiveButton("Add") { _, _ ->
+                    Log.d("debug", "adding!")
+                    doAsync {
+                        if(!(dialogView.name.parseString("").isEmpty()) && viewModel.getExercise(dialogView.name.text.toString()) == null) {
+                            addExercise()
+                            recycleAdapter.setData(viewModel.getAllExercises())
+                            Log.d("d", "added ${dialogView.name.text}")
+                        } else
+                        {
+                            Log.d("d", "didn't add")
+                        }
+                    }
+
+                }
+
+                .setNegativeButton("Cancel"
+                ) {
+                    dialogBox, _ -> dialogBox.cancel()
+                }
+                .create()
+                .show()
+        dialogView.showKeyboard()
+    }
+
+    private fun deleteExercise(name:String){
+        context!!.alert("Are you sure?", "Deleting exercise") {
+            yesButton { viewModel.deleteExercise(name) }
+            noButton {}
+        }.show()
+    }
+
+
+    private fun addExercise(){
+        with(dialogView){
+            viewModel.addExercise(
+                    name.parseString(""),
+                    reps.parseString("5").toInt(),
+                    num_sets.parseString("3").toInt(),
+                    upper.isChecked,
+                    increment.parseString(when(upper.isChecked){
+                        true -> "5.0"
+                        false -> "2.5"
+                    }).toDouble(),
+                    starting_weight.parseString("100").toDouble()
+            )
+        }
+    }
 }
