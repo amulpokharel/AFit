faf55f9a2478795b5e69249f67271a77d195e68e
Amul Pokharel <amulpokh@gmail.com>
2018-09-28 16:54:27 -0400

    added multiple classes to the db

--- app/src/main/java/amulp/com/afit/db/MainDb.kt
+++ app/src/main/java/amulp/com/afit/db/MainDb.kt
@@ -1,12 +1,13 @@
 package amulp.com.afit.db
 
 import amulp.com.afit.models.Routine
+import amulp.com.afit.models.Exercise
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Routine::class], version = 1, exportSchema = false)
+@Database(entities = [Routine::class, Exercise::class], version = 1, exportSchema = false)
 abstract class RoutineDb : RoomDatabase() {
 
     abstract fun routineDao(): RoutineDao
